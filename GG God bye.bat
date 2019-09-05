b955233618dc76b8bc78ab38ac705a06db79d2fe
CoderMan5821 <54962944+CoderMan5821@users.noreply.github.com>
2019-09-05 17:51:25 -0400

    Add files via upload

---
+++ GG God bye.bat
@@ -0,0 +1,77 @@
+echo.
+echo BEGINNING DECRYPTION ALGORYTHM
+echo.
+wait 5
+cls
+echo.
+echo.
+echo.
+
+echo "error code 404. See service Provider for Details. Press any key to respond.
+echo.
+echo.
+pause
+GOTO Menu
+
+:Menu
+echo Execute Shutdown Procedure?
+ECHO.
+ECHO ...............................................
+ECHO PRESS 1, 2 OR 3 to select your task, or 4 to EXIT.
+ECHO ...............................................
+ECHO.
+ECHO 1 - Yes
+ECHO 2 - No
+ECHO 3 - Screw Off
+ECHO 4 - EXIT
+ECHO.
+
+SET /P M=Type 1, 2, 3, or 4 then press ENTER:
+IF %M%==1 GOTO Yes
+IF %M%==2 GOTO No
+IF %M%==3 GOTO Screw Off
+IF %M%==4 GOTO EOF
+
+:YES
+cls
+echo "Ok. Complying"
+pause
+/s /t 60 /c "Shutdown in progress, leave the vicinity immediately"
+eof
+
+:No
+echo "Are you sure?"
+ECHO.
+ECHO ...............................................
+ECHO PRESS 1, 2 OR 3 to select your task, or 4 to EXIT.
+ECHO ...............................................
+ECHO.
+ECHO 1 - Yes
+ECHO 2 - Yes
+echo. 
+
+SET /P M=Type 1, 2, 3, or 4 then press ENTER:
+IF %M%==1 GOTO YES
+IF %M%==2 GOTO Yes
+
+:Screw Off
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
+Echo No You
